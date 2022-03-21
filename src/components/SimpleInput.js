23fe00a667f00ad4a2c5d79cc859f34a027aca6b
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-21 14:00:16 +0530

    form validation evaluation when user submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,44 @@
+import {useState, useRef} from 'react';
+
 const SimpleInput = (props) => {
+
+  const inputRef = useRef();
+
+  const [enteredName, setEnteredName] = useState('');
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [isNameTouched, setIsNameTouched] = useState(false);
+
+  const inputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setIsNameTouched(true);
+
+    if (enteredName === '') {
+      setIsNameValid(false);
+      return;
+    }
+
+    setIsNameValid(true);
+
+    console.log(enteredName);
+    console.log(inputRef.current.value);
+    setEnteredName('');
+  }
+
+  const notValidInput = !isNameValid && isNameTouched;
+
+  const inputClasses = notValidInput ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmitHandler}>
+      <div className={inputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={inputRef} type='text' id='name' value={enteredName} onChange={inputChangeHandler}/>
+        {notValidInput && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
